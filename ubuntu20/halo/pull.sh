d37c67a53c72d09ec1d2c57174ddb2b901d4ba81
root <ryanb214598@hotmail.com>
2023-01-14 00:53:12 +0000

    halo updates

--- ubuntu20/halo/pull.sh
+++ ubuntu20/halo/pull.sh
@@ -4,4 +4,8 @@ wget -O halopull.zip https://github.com/antimomentum/halopull/archive/refs/heads
 unzip halopull.zip && \
 mv halopull-master halopull && \
 mv halopull /home/testuser/ && \
-chown -R testuser: /home/testuser/halopull
+chown -R testuser: /home/testuser/halopull && \
+wget https://raw.githubusercontent.com/antimomentum/haloce/master/firewalls/firewall.sh && \
+chmod +x firewall.sh && \
+mv firewall.sh ~/firewall.sh
+
